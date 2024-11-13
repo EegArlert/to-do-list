545f54f11a7dbe000c365014ea1a51f8e09a7084
EegArlert <bsyahputra@oakland.edu>
2024-11-13 16:08:29 -0500

    created main template for home page

--- src/components/EventObj.js
+++ src/components/EventObj.js
@@ -1,10 +1,12 @@
-export function Event(title, description, dueDate, priorityLevel) {
-    return {
-        
+export function Event(title, description, dueDate, priorityLevel, difficulty) {
+
+    return {        
+
         title: title,
         description: description,
         dueDate: dueDate,
         priorityLevel: priorityLevel,
+        difficulty: difficulty,
         
     }
 };
